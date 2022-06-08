baeb92f3dc5e6485f026c3f0942b88a9ab76cc6c
Tosin Oseni <oluwatosinoseni@gmail.com>
2022-06-08 21:05:50 +0100

    back end

---
+++ graph_slam_scratch/graphSlam/icp.py
@@ -0,0 +1,9 @@
+import math
+import  numpy as np
+
+class ICP():
+    def __init__(self) -> None:
+        pass
+
+    def find_transform_scan_mathching(self, scan_a, scan_b,iterations=100, tolerance=1):
+        
